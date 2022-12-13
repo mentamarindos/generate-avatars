f006b07f7d19c7810862ceb03444022b61b3e05c
mentamarindos <mentamarindos@github.com>
2022-12-13 15:24:04 -0600

    initial commit

---
+++ src/components/Form.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const file = ref();
+
+</script>
+
+ <!-- template  -->
+<template>
+
+<h1>Avatar generator using Python</h1>
+
+<form>
+  <!-- Grid -->
+  <div class="grid">
+    <label for="upload">
+      Upload a photo
+      <input type="file" id="upload" name="file" required> 
+    </label>
+  </div>
+  <!-- Grid -->
+  <div class="grid">
+    <h3>Result</h3>
+  </div>
+</form>
+
+</template>
